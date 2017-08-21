facdfcfa01303f37b662c53f6e1ad8d694cd1429
Alexandru Toader | eMAG <alexandru.toader@emag.ro>
2017-08-21 15:47:33 +0300

    Adding admin return query.

--- classes/departments/departments.php
+++ classes/departments/departments.php
@@ -21,7 +21,11 @@ require_once "../../classes/dbconnect/dbconnect.php";
             $db = new \SD_app\DbConnection();
             $db->query("SELECT * FROM manager");
             $result = $db->resultset();
-            echo "<table style='border:1px solid black;'><tr><th>" . $result[0]['rang']. "</th></tr>" . "<tr><td>" . $result[0]['nume_manager'] . "</td></tr><br>";
+            echo "<table style='border:1px solid black;'><tr>" . "" . "<th>Id_manager</th>" . "" . "<th>Id_departament</th>" . "" . "<th>Rang</th>" . "" . "<th>Nume_manager</th>" . "" . "<th>Numar_subalterni</th></tr>";
+            foreach($result as $output) {
+                echo "</td><td>" . $output['id_manager'] . "</td><td>" . $output['id_departament'] . "</td><td>" . $output['rang'] . "</td><td>" . $output['nume_manager'] . "</td><td>" . $output['numar_subalterni'] . "</td></tr>";
+            }
+            echo "</table><br>";
             echo "</table>";
             ?>
         </tr>
@@ -33,7 +37,7 @@ require_once "../../classes/dbconnect/dbconnect.php";
         foreach($result as $output) {
             echo "</td><td>" . $output['name'] . "</td><td>" . $output['surname'] . "</td><td>" . $output['echipa'] . "</td><td>" . $output['user'] . "</td><td>" . $output['email'] . "</td></tr>";
         }
-        echo "</table><br>";
+        echo "</table>";
         ?>
     </table>
 </div>
