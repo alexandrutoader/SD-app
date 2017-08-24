60330acc9ef3267fdeec634d69075e391129f57d
Alexandru Toader | eMAG <alexandru.toader@emag.ro>
2017-08-24 11:00:19 +0300

    Adding code regarding the delete user.

--- classes/delete/delete_user.php
+++ classes/delete/delete_user.php
@@ -2,8 +2,14 @@
 class Delete_user {
     public function delete(){
         $sql = new \SD_app\DbConnection();
-            if(isset($_POST['delete'])){
-                $sql->query("DELETE FROM manager WHERE ");
+        $id = $_POST['id_manager'];
+        $delete = $_POST['delete'];
+            if(isset($delete)){
+                $sql->query("DELETE FROM manager WHERE id_manager='" . $id."'");
+                $del = $sql->resultset();
+                var_dump($del);
+            } else {
+                echo 'okkkkkasdasd';
             }
     }
 }
