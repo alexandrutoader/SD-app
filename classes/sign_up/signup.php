e4fddcdd27c5398b840eb79f3db8d5679ab59773
Alexandru Toader | eMAG <alexandru.toader@emag.ro>
2017-08-28 14:57:53 +0300

    Adding extra conditions on sign up.

--- classes/sign_up/signup.php
+++ classes/sign_up/signup.php
@@ -13,9 +13,13 @@ class SignUp {
             $checkemail = $sql->resultset();
             $sql->query("SELECT * FROM users WHERE user='$username'");
             $checkuser = $sql->resultset();
+            $mail = isset($checkemail[0]['email']) ? $checkemail[0] : '';
+
             if ($name == '' || $surname == '' || $username == '' || $password == '') {
-                echo "<h3 style='text-align: center; margin-top: 100px;'>Please provide your name, surname, username, password, recovery password and team.</h3>";
-            } elseif($checkemail[0] > 1){
+                echo "<h3 style='text-align: center; margin-top: 100px;'>Please provide your name, surname, username, password and team.</h3>";
+            } elseif (count($mail) > 1 && $checkuser[0] > 1){
+                echo "<script>alert('Email and user already exists! Please type another email and user!')</script>";
+            } elseif(count($mail) > 1){
                 echo "<script>alert('Email already exists! Please type another email!')</script>";
             } elseif($checkuser[0] > 1){
                 echo "<script>alert('User already exists! Please select another user!')</script>";
