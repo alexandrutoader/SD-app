86bcd76c32c8bf215b695d3fc8ad395fb6887a51
Alexandru Toader | eMAG <alexandru.toader@emag.ro>
2017-08-30 11:18:25 +0300

    master: column users modified on db

--- db.sql
+++ db.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `angajati` (
   PRIMARY KEY (`id`),
   KEY `Index 3` (`nume_echipa`),
   KEY `Index 2` (`name`,`surname`,`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table sd_app.angajati: ~18 rows (approximately)
 /*!40000 ALTER TABLE `angajati` DISABLE KEYS */;
@@ -46,7 +46,10 @@ INSERT IGNORE INTO `angajati` (`id`, `name`, `surname`, `nume_echipa`) VALUES
 	(15, 'Bogdan', 'Tudorel', 'QC'),
 	(16, 'Bogdan', 'Tud', ''),
 	(17, 'Dorel', 'Dobrescu', ''),
-	(18, 'Dorel', 'Dobrescu', 'OM2');
+	(18, 'Dorel', 'Dobrescu', 'OM2'),
+	(19, 'Ionut', 'Popescu', 'Team CC'),
+	(20, 'Adrian', 'Eftimie', 'Team Marketing'),
+	(21, 'Ana', 'Burleanu', 'OM2');
 /*!40000 ALTER TABLE `angajati` ENABLE KEYS */;
 
 
@@ -110,15 +113,18 @@ CREATE TABLE IF NOT EXISTS `manager` (
   UNIQUE KEY `Index 2` (`rang`),
   KEY `FK_manager_departament` (`id_departament`,`nume_manager`),
   CONSTRAINT `FK_manager_departament` FOREIGN KEY (`id_departament`) REFERENCES `departament` (`id_departament`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table sd_app.manager: ~4 rows (approximately)
 /*!40000 ALTER TABLE `manager` DISABLE KEYS */;
 INSERT IGNORE INTO `manager` (`id_manager`, `id_departament`, `rang`, `nume_manager`, `numar_subalterni`) VALUES
-	(3, 1, 'Manager OM', 'Ciprian Iosif', 4),
-	(6, 6, 'Team Leader QA', 'Dumitru Andreescu', 10),
-	(7, 6, 'Team Leader QC', 'Ana Gabriela', 10),
-	(11, 7, 'Team Leader SCM', 'Andrei Dobre', 10);
+	(1, 7, 'Team Leader SCM', 'Andrei Damian', 10),
+	(2, 6, 'Manager OM', 'Ciprian Iosif', 4),
+	(3, 3, 'Team Leader HR', 'Dumitru Andreescu', 10),
+	(4, 5, 'Team Leader QC', 'Ana Gabriela', 12),
+	(5, 1, 'Team Leader Call Center', 'Andreea Ionescu', 80),
+	(6, 2, 'Team Leader Documentare', 'Adrian Gheorghe', 25),
+	(7, 4, 'Team Leader Marketing', 'Bogdan Grigore', 16);
 /*!40000 ALTER TABLE `manager` ENABLE KEYS */;
 
 
@@ -137,24 +143,26 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `Index 2` (`email`,`user`),
   KEY `FK_users_angajati` (`name`,`surname`),
   KEY `Index 4` (`echipa`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
--- Dumping data for table sd_app.users: ~13 rows (approximately)
+-- Dumping data for table sd_app.users: ~12 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT IGNORE INTO `users` (`id`, `name`, `surname`, `echipa`, `user`, `email`, `password`, `user_type`, `created`) VALUES
-	(3, 'Ionut', 'Cristescu', 'OM1', 'ionut.cristescu', 'ionut.cristescu@emag.ro', '2f7e6fcc8598ebefd291892090027042', 'USER', '2017-08-29 12:37:11'),
-	(4, 'Ana', 'Costea', 'HR', 'ana.costea', 'ana.costea@emag.ro', '43177dfb9750fe9007716a35f3512325', 'USER', '2017-08-29 12:37:31'),
-	(5, 'Daniel', 'Dumitru', 'SCM', 'daniel.dumitru', 'daniel.dumitru@emag.ro', '66f3c0ba74813dab8080eed1c871d26e', 'USER', '2017-08-29 12:37:48'),
-	(6, 'Adrian', 'Ionescu', 'OM2', 'adrian.ionescu', 'adrian.ionescu@emag.ro', '8519ae1aeb8670a63402749d4f3683e5', 'ADMIN', '2017-08-29 17:53:24'),
-	(7, 'Ciprian', 'Andrei', 'Team Doc', 'ciprian.andrei', 'ciprian.andrei@emag.ro', '66f3c0ba74813dab8080eed1c871d26e', 'USER', '2017-08-29 12:37:48'),
-	(8, 'Maria', 'Ionescu', 'OM2', 'maria.ionescu', 'maria.ionescu@emag.ro', '53f8db2e4934c145e168c6c74b34fc2c', 'USER', '2017-08-30 09:30:55'),
-	(9, 'Alex', 'Florescu', 'Team Marketing', 'alex.florescu', 'alex.florescu@emag.ro', 'cfd26e86648d719b8f6409f8a3b46396', 'USER', '2017-08-30 09:34:30'),
-	(10, 'Ciprian', 'Teodor', 'Team CC', 'ciprian.teodor', '', '4006fdb0d5628e361883829825749474', 'USER', '2017-08-30 09:35:57'),
-	(11, 'Ioana', 'Dobre', 'QC', 'ioana.dobre', 'ioana.dobre@emag.ro', 'eafe30125b40e3ef117178c8ddc9739f', 'USER', '2017-08-30 09:36:33'),
-	(12, 'Dorel', 'Mihai', 'HR', 'dore.mihai', 'alex@dgsdg.ro', '7b01af214719660fb85ec69a412ebd26', 'USER', '2017-08-30 09:39:19'),
-	(13, 'Dan', 'Stoica', 'OM2', 'dan.stoica', 'dan.stoica@emag.ro', 'ec1171fa6457ea2d022bc93505a1305b', 'USER', '2017-08-30 09:39:42'),
-	(14, 'Dorel', 'Dobrescu', 'SCM', 'dorel.dobrescu', 'dorel.dobrescu@emag.ro', 'a1788285e331b8ec67f255c32d3efff0', 'USER', '2017-08-30 10:33:59'),
-	(27, 'Ciprian', 'Tudose', 'SCM', 'ciprian.tudose', 'ciprian.tudose@emag.ro', 'e146390a2f2f38ff198bc130fe1013cf', 'USER', '2017-08-30 10:41:02');
+	(1, 'Ionut', 'Cristescu', 'OM1', 'ionut.cristescu', 'ionut.cristescu@emag.ro', '2f7e6fcc8598ebefd291892090027042', 'USER', '2017-08-30 11:15:24'),
+	(2, 'Ana', 'Costea', 'HR', 'ana.costea', 'ana.costea@emag.ro', '43177dfb9750fe9007716a35f3512325', 'USER', '2017-08-30 11:15:25'),
+	(3, 'Daniel', 'Dumitru', 'SCM', 'daniel.dumitru', 'daniel.dumitru@emag.ro', '66f3c0ba74813dab8080eed1c871d26e', 'USER', '2017-08-30 11:15:26'),
+	(4, 'Adrian', 'Ionescu', 'OM2', 'adrian.ionescu', 'adrian.ionescu@emag.ro', '8519ae1aeb8670a63402749d4f3683e5', 'ADMIN', '2017-08-30 11:15:27'),
+	(5, 'Ciprian', 'Andrei', 'Team Doc', 'ciprian.andrei', 'ciprian.andrei@emag.ro', '66f3c0ba74813dab8080eed1c871d26e', 'USER', '2017-08-30 11:15:27'),
+	(6, 'Maria', 'Ionescu', 'OM2', 'maria.ionescu', 'maria.ionescu@emag.ro', '53f8db2e4934c145e168c6c74b34fc2c', 'ADMIN', '2017-08-30 11:15:28'),
+	(7, 'Alex', 'Florescu', 'Team Marketing', 'alex.florescu', 'alex.florescu@emag.ro', 'cfd26e86648d719b8f6409f8a3b46396', 'USER', '2017-08-30 11:15:29'),
+	(8, 'Ciprian', 'Teodor', 'Team CC', 'ciprian.teodor', '', '4006fdb0d5628e361883829825749474', 'USER', '2017-08-30 11:15:30'),
+	(9, 'Ioana', 'Dobre', 'QC', 'ioana.dobre', 'ioana.dobre@emag.ro', 'eafe30125b40e3ef117178c8ddc9739f', 'USER', '2017-08-30 11:15:31'),
+	(10, 'Dorel', 'Mihai', 'HR', 'dorel.mihai', 'alex@dgsdg.ro', '5c6142ed6c39080a842a66121582ad52', 'USER', '2017-08-30 11:15:34'),
+	(11, 'Dan', 'Stoica', 'OM2', 'dan.stoica', 'dan.stoica@emag.ro', 'ec1171fa6457ea2d022bc93505a1305b', 'USER', '2017-08-30 11:15:35'),
+	(12, 'Dorel', 'Dobrescu', 'SCM', 'dorel.dobrescu', 'dorel.dobrescu@emag.ro', 'a1788285e331b8ec67f255c32d3efff0', 'USER', '2017-08-30 11:15:36'),
+	(13, 'Ionut', 'Popescu', 'Team CC', 'ionut.popescu', 'ionut.popescu@emag.ro', 'cd9ca138bd1872a24e386acf489fd3f7', 'USER', '2017-08-30 11:15:37'),
+	(14, 'Adrian', 'Eftimie', 'Team Marketing', 'adrian.eftimie', 'adrian.eftimie@emag.ro', 'cd9ca138bd1872a24e386acf489fd3f7', 'USER', '2017-08-30 11:15:38'),
+	(17, 'Ana', 'Burleanu', 'OM2', 'ana.burleanu', 'ana.burleanu@emag.ro', 'ed72e03657ed8a1087c4365a85dfc095', 'USER', '2017-08-30 11:16:14');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
